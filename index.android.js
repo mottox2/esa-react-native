b0c1591d282f857ac777d5d2afe658c2bfe4d29c
mottox2 <y.mottox2@gmail.com>
2017-01-01 23:39:22 +0900

    Request accessToken

--- index.android.js
+++ index.android.js
@@ -23,11 +23,15 @@ export default class esaReactNative extends Component {
     this.state = {
       authorized: true
     }
+    Linking.addEventListener('url', event => this.handleOpenURL(event))
   }
   async componentDidMount() {
     console.log('componentDidMount')
     var accessToken = await store.get('accessToken')
     if (!accessToken) this.setState({ authorized: false })
+  }
+  componentWillUnmount() {
+    Linking.removeEventListener('url');
   }
   authorize() {
     Linking.openURL([
@@ -36,6 +40,29 @@ export default class esaReactNative extends Component {
       '&client_id=' + Config.CLIENT_ID,
       '&redirect_uri=' + Config.REDIRECT_URI,
     ].join(''))
+  }
+  async handleOpenURL(event) {
+    const authorizationCode = event.url.match(/\?code=(.*)/)[1]
+    const requestData = {
+      client_id: Config.CLIENT_ID,
+      client_secret: Config.CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      redirect_uri: Config.REDIRECT_URI,
+      code: authorizationCode,
+    }
+    let headers = new Headers();
+    headers.append('Accept', 'application/json')
+    headers.append('Content-Type', 'application/json')
+
+    const response = await fetch('https://api.esa.io/oauth/token', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(requestData),
+    })
+    const responseJson = await response.json()
+    console.log(responseJson)
+    await store.save('access_token', responseJson.access_token)
+    this.setState({authorized: true})
   }
   render() {
     return (
