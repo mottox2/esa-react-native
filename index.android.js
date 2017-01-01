4d677904bf57ac501723fff5df8d1625da080335
mottox2 <y.mottox2@gmail.com>
2017-01-01 23:19:25 +0900

    Open authorization url

--- index.android.js
+++ index.android.js
@@ -9,17 +9,44 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
+  Linking,
 } from 'react-native';
 import Config from './config.js'
+import store from 'react-native-simple-store';
+import Frisbee from 'frisbee';
 
 export default class esaReactNative extends Component {
-  componentDidMount(props) {
+  constructor(props) {
     super(props)
+    this.state = {
+      authorized: true
+    }
+  }
+  async componentDidMount() {
+    console.log('componentDidMount')
+    var accessToken = await store.get('accessToken')
+    if (!accessToken) this.setState({ authorized: false })
+  }
+  authorize() {
+    Linking.openURL([
+      'https://api.esa.io/oauth/authorize',
+      '?response_type=code',
+      '&client_id=' + Config.CLIENT_ID,
+      '&redirect_uri=' + Config.REDIRECT_URI,
+    ].join(''))
   }
   render() {
     return (
       <View style={styles.container}>
+        { this.state.authorized ?
+          <Text>Authorized</Text> :
+          <View>
+            <Text> Not Authorized </Text>
+            <Button title='Authorize' onPress={this.authorize}/>
+          </View>
+        }
       </View>
     );
   }
