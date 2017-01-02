fd0eabec79651dc5c3962faeb1fb5134158e35d1
mottox2 <y.mottox2@gmail.com>
2017-01-03 01:06:42 +0900

    Fix style

--- index.android.js
+++ index.android.js
@@ -21,16 +21,21 @@ import PostListView from './src/components/PostListView.js'
 
 export default class esaReactNative extends Component {
   constructor(props) {
+    console.disableYellowBox = true;
+
     super(props)
     this.state = {
-      authorized: false
+      authorized: true, //false
+      isLoading: true,
     }
     Linking.addEventListener('url', event => this.handleOpenURL(event))
   }
   async componentDidMount() {
     console.log('componentDidMount')
     var accessToken = await store.get('accessToken')
-    if (accessToken) this.setState({ authorized: true })
+
+    if (accessToken) this.setState({ authorized: true, isLoading: false })
+    else this.setState({ authorized: false, isLoading: false})
   }
   componentWillUnmount() {
     Linking.removeEventListener('url');
@@ -64,16 +69,21 @@ export default class esaReactNative extends Component {
     const responseJson = await response.json()
     console.log(responseJson)
     await store.save('accessToken', responseJson.access_token)
-    this.setState({authorized: true})
+    this.setState({authorized: true, isLoading: false})
   }
   render() {
     return (
       <View style={styles.container}>
-        { this.state.authorized ?
-          <PostListView/> :
-          <View>
-            <Text> Not Authorized </Text>
-            <Button title='Authorize' onPress={this.authorize}/>
+        { this.state.isLoading ?
+          <View><Text>Loading...</Text></View> :
+          <View style={styles.container}>
+            { this.state.authorized ?
+              <PostListView/> :
+              <View>
+                <Text> Not Authorized </Text>
+                <Button title='Authorize' onPress={this.authorize}/>
+              </View>
+            }
           </View>
         }
       </View>
