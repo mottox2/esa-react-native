c296ddea4473744aa08fcb394124df4699e41bd2
mottox2 <y.mottox2@gmail.com>
2017-01-04 01:13:56 +0900

    Add style to html

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -14,12 +14,45 @@ const api = new Frisbee({
   baseURI: 'https://api.esa.io'
 })
 
-const htmlStyles = `
+let htmlStyles = `
 <style>
 a.anchor {
   display: none
 }
+a {
+  color: #0a9b94;
+  text-decoration: none;
+}
+body {
+  overflow: auto;
+  color: #3c4a60;
+}
+pre {
+  overflow: auto;
+  white-space: pre;
+  word-wrap: break-word;
+  word-break: break-all;
+  background-color: #f5f5f5;
+  display: block;
+  padding: 20px;
+}
+iframe, img {
+  max-width: 100%;
+}
+li {
+  margin: 4px 0;
+}
+h2 {
+  font-weight: bold;
+  border-bottom: 2px solid #ddd;
+  padding: 8px 0;
+}
+h2 .fa, h3 .fa, h4 .fa {
+  color: rgba(60,74,96,0.3);
+  margin-right: 4px;
+}
 </style>
+<script src="https://use.fontawesome.com/ffb71e0a08.js"></script>
 `
 
 export default class DetailScreen extends Component {
@@ -46,7 +79,7 @@ export default class DetailScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <WebView style={styles.webView} source={{ html: htmlStyles + this.props.route.params.body_html }} />
+        <WebView style={styles.webView} source={{ html: `${htmlStyles}<body>${this.props.route.params.body_html}</body>` }} />
       </View>
     )
   }
