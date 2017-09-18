3e130fc049e37579e092241e0dec94ba40432473
mottox2 <y.mottox2@gmail.com>
2017-09-18 11:09:53 +0900

    Get access_token

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, Linking, Button } from 'react-native';
+import store from 'react-native-simple-store';
+import Frisbee from 'frisbee';
 import Config from './config.js'
+
+const api = new Frisbee({
+  baseURI: 'https://api.esa.io'
+})
 
 export default class App extends React.Component {
   componentDidMount() {
     Linking.getInitialURL().then((url) => {
       if (url) {
         console.log('Initial url is: ' + url);
+        this.handleOpenURL(url)
       }
     })
     Linking.addEventListener('url', this.handleOpenURL)
@@ -22,8 +29,39 @@ export default class App extends React.Component {
 
   }
 
-  handleOpenURL(event) {
-    console.log(event.url)
+  async handleOpenURL(url) {
+    const authorizationCode = url.match(/\?code=(.*)/)[1]
+    const requestData = {
+      client_id: Config.CLIENT_ID,
+      client_secret: Config.CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      redirect_uri: Config.REDIRECT_URI,
+      code: authorizationCode,
+    }
+    let headers = new Headers();
+    headers.append('Accept', 'application/json')
+    headers.append('Content-Type', 'application/json')
+
+    const response = await fetch('https://api.esa.io/oauth/token', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(requestData),
+    })
+    const responseJson = await response.json()
+    console.log(responseJson)
+    if (responseJson.error) {
+      console.log(responseJson)
+      return
+    }
+    const accessToken = responseJson.access_token
+    await store.save('accessToken', accessToken)
+    const user = await api.jwt(accessToken).get('/v1/user?include=teams')
+    await store.save('user', user.body)
+    if (user.body.teams.length == 0) {
+      console.log('閲覧出来る記事がありません。esa.ioへの登録が必要です。')
+    } else {
+      await store.save('teamName', user.body.teams[0].name)
+    }
   }
 
   render() {
